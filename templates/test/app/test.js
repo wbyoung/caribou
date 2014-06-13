f5823e5e9b78188f4399bf786bcab3650e1800d4
Whitney Young <wbyoung@fadingred.com>
2014-06-13 12:54:32 -0700

    More clear example test.

--- templates/test/app/test.js
+++ templates/test/app/test.js
@@ -1,10 +1,12 @@
 /* global Application, describe, it, expect */ 'use strict';
 
 describe('app', function() {
-  it('passes tests', function() {
-    var post = Application.controllers.posts.create();
-    expect(post).to.to.have.property('id');
-    expect(post).to.to.have.property('title');
-    expect(post).to.to.have.property('body');
+  describe('posts controller', function() {
+    it('creates posts', function() {
+      var post = Application.controllers.posts.create();
+      expect(post).to.to.have.property('id');
+      expect(post).to.to.have.property('title');
+      expect(post).to.to.have.property('body');
+    });
   });
 });
